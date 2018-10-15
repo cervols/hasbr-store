928fdf3d26cc522b8376c60c39cc7e7638589352
cervols <paprikamail@gmail.com>
2018-10-15 18:28:16 +0300

    Episodes 24-26 - Partials. Helpers. Layouts

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,10 @@
+class AdminController < ApplicationController
+
+  before_action :check_if_admin
+  layout "admin"
+
+  def users_count
+    @users_count = User.count
+    # render layout: "admin"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
-  
+  root to: 'items#index'
+
+  devise_scope :user do
+    get "/users/sign_out", to: "devise/sessions#destroy"
+  end
+
   devise_for :users
 
   resources :items do
     get :upvote,    on: :member
     get :expensive, on: :collection
   end
+
+  get "admin/users_count", to: "admin#users_count"
 end
